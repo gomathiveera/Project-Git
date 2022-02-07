285649ce621496d31b51569fd58e41e01d881bb4
gomathiveera <gomathiveera2210@gmail.com>
2022-02-07 13:56:05 -0800

    gomathi task done

--- src/org/git/Employee.java
+++ src/org/git/Employee.java
@@ -4,7 +4,14 @@ public class Employee {
 	
 	private void Address() {
 		System.out.println("Emp address");
+		
+		System.out.println("emp id+ ph no");
 
+	}
+	public static void main(String[] args) {
+		Employee e  = new Employee();
+		e.Address();
+		
 	}
 
 }
